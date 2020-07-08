fe6cdbe13b2746fd69a917616054f6514bbfd524
Fernando Timo <fertimo99@gmail.com>
2020-07-08 10:08:35 -0500

    Pruebas de git

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.route('/').get((req, res) => {
-  res.json({ saludo: 'carecaca' });
+  res.json({ saludo: 'Hola Mundo' });
 });
 
 export default routes;
