92fa93572ad81883fafbe9caa3c89cd2ae6baab8
Fernando Timo <fertimo99@gmail.com>
2020-07-08 12:45:32 -0500

    Conectado a DB de Mongo Atlas

--- src/Controllers/get.js
+++ src/Controllers/get.js
@@ -2,6 +2,10 @@ import User from '../Models/user';
 let get = {};
 
 get.user = async (req, res) => {
+  let user = new User({
+    name: 'FernandoTimo',
+  });
+  user.save();
   const users = await User.find();
   // res.send('calla get');
   res.json(users);
