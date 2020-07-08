84b77759b7ba491dcf53094da88f36f780bf7ccd
Fernando Timo <fertimo99@gmail.com>
2020-07-08 13:40:04 -0500

    ahora si ya fuinciono, probando operadores ternarios

--- src/Controllers/get.js
+++ src/Controllers/get.js
@@ -2,13 +2,17 @@ import User from '../Models/User';
 let get = {};
 
 get.user = async (req, res) => {
+  const okey = true;
+  const badey = false;
+
   let user = new User({
     name: 'FernandoTimo',
   });
   user.save();
   const users = await User.find();
   // res.send('calla get');
-  res.json(users);
+  okey ? res.json(users) : res.json({ respuesta: 'todo salio mal :D jojoi' });
+  badey ? res.json({ respuesta: 'todo salio mal :D jojoi' }) : res.json(users);
 };
 
 export default get;
