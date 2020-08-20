9ae2b6e4a96618af8835db34fce1335d2f2c2d26
FernandoTimo <fertimo99@gmail.com>
2020-08-19 22:07:24 -0500

    Nuevo Socket

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,12 +1,14 @@
 import { listen } from 'socket.io';
 
-const sockets = (server) => {
-  const socket = listen(server, { path: '/timoideas' });
+const socketsMain = (socket) => {
   socket.on('connection', (client) => {
     console.log('Cliente conectado');
     client.on('disconnect', () => {
       console.log('Cliente desconectado');
     });
   });
+};
+const sockets = (server) => {
+  socketsMain(listen(server, { path: '/timoideas' }));
 };
 export default sockets;
