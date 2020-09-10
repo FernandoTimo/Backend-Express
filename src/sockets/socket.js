3764f77a4374d151b9cbaa97b2e517f0e7e6aeef
FernandoTimo <Fertimo99@gmail.com>
2020-09-09 22:57:12 -0500

    Store Sockets

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -6,6 +6,18 @@ const socketsMain = (socket) => {
     client.on('disconnect', () => {
       console.log('--');
     });
+    // STORE --------------------------
+    // STORE --------------------------
+    // STORE --------------------------
+    // STORE --------------------------
+    // STORE --------------------------
+    client.on('store-comprobante', (comprobante) => {
+      console.log(comprobante);
+      socket.emit('store-comprobante_recivido', 'recivido');
+    });
+    client.on('store-comprobante_validado', (StoreCode) => {
+      socket.emit('store-comprobante_validado', StoreCode);
+    });
   });
 };
 const sockets = (server) => {
