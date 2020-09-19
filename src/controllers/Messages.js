3b8bd3fbd35766c5dcc81cacbb03c67aee9900c3
Timoideas <Timoideas@hotmail.com>
2020-09-19 11:51:58 -0500

    Messages

---
+++ src/controllers/Messages.js
@@ -0,0 +1,3 @@
+export async function Messages(req, res) {
+  console.log(req);
+}
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,5 +1,9 @@
 import { listen } from 'socket.io';
-
+import twilio from 'twilio';
+const clienteTwilio = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 const socketsMain = (socket) => {
   socket.on('connection', (client) => {
     console.log(client.id);
@@ -12,7 +16,17 @@ const socketsMain = (socket) => {
     // STORE --------------------------
     // STORE --------------------------
     client.on('store-comprobante', (comprobante) => {
-      console.log(comprobante);
+      clienteTwilio.messages
+        .create({
+          body:
+            'Hola Benjamin, acabamos de recibir tu pedido, no te lo enviaremos nunca, esto es solo para que sepas que fuiste vilmente estafado',
+          from: 'whatsapp:+14155238886',
+          to: `whatsapp:${process.env.TWILIO_NUMBER_SENDER}`,
+          // from: '+13345084568',
+          // to: 'whatsapp:+51983443031',
+        })
+        .then((res) => console.log(res))
+        .done();
       socket.emit('store-comprobante_recivido', 'recivido');
     });
     client.on('store-comprobante_validado', (StoreCode) => {
