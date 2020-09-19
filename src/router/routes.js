7bc876de830332b507bf7dd08f515aeb787513ed
Timoideas <Timoideas@hotmail.com>
2020-09-19 11:56:05 -0500

    Ngrok

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 const routes = Router();
 
 import { Index, BadUrl } from '../controllers/controller';
+import { Messages } from '../controllers/Messages';
 import { Auth } from '../middlewares/Auth';
 routes.route('/').get(Auth, Index);
+routes.route('/Messages').get(Messages);
 routes.route('*').get(BadUrl);
 
 export default routes;
