f29e26802604d4ec4086a5007cc4706dc09bb2b9
Timoideas <Timoideas@hotmail.com>
2020-09-19 12:05:43 -0500

    Data Messages Recive body msg

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ import { Index, BadUrl } from '../controllers/controller';
 import { Messages } from '../controllers/Messages';
 import { Auth } from '../middlewares/Auth';
 routes.route('/').get(Auth, Index);
-routes.route('/Messages').get(Messages);
+routes.route('/Messages').post(Messages);
 routes.route('*').get(BadUrl);
 
 export default routes;
