b20c505118ec5072e5a5aa5de02332ca1d466edb
Timoideas <Timoideas@hotmail.com>
2020-09-19 16:28:45 -0500

    Sockets env

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -22,11 +22,13 @@ const socketsMain = (socket) => {
             'Hola Benjamin, acabamos de recibir tu pedido, no te lo enviaremos nunca, esto es solo para que sepas que fuiste vilmente estafado',
           from: 'whatsapp:+14155238886',
           to: `whatsapp:${process.env.TWILIO_NUMBER_SENDER}`,
+          mediaUrl:
+            'https://instagram.faqp2-1.fna.fbcdn.net/v/t51.2885-15/e35/119065834_371305847368302_256366898104219625_n.jpg?_nc_ht=instagram.faqp2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=7RdajyCUBoEAX_SBLlS&_nc_tp=18&oh=ade63601a217751b48a05fa52cd01ac1&oe=5F90EE96',
           // from: '+13345084568',
           // to: 'whatsapp:+51983443031',
         })
         .then((res) => console.log(res))
-        .done();
+        .catch((err) => console.log(err));
       socket.emit('store-comprobante_recivido', 'recivido');
     });
     client.on('store-comprobante_validado', (StoreCode) => {
