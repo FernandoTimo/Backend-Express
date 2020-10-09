5f2f47be15e9fda019d234dc8c70a5627fd91370
FernandoTimo <Fertimo99@gmail.com>
2020-10-09 11:36:37 -0500

    Libraries

--- src/sockets/connnectSockets.js
+++ src/sockets/connnectSockets.js
@@ -1,5 +1,19 @@
 import { socket } from './socket';
 
 socket.on('connection', (client) => {
-  console.log(client.id);
+  console.log('\x1b[31mNuevoCliente:\x1b[0m', client.id);
+
+  function getRandom(arr, n) {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError('getRandom: more elements taken than available');
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+  }
 });
