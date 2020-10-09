9f72861415981be52f9805d754965ec69f66d88f
FernandoTimo <Fertimo99@gmail.com>
2020-10-09 13:04:01 -0500

    Socket client connection message

--- src/sockets/clientConnection.js
+++ src/sockets/clientConnection.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 socket.on('connection', (client) => {
   console.log(`\x1b[31mNuevoCliente:\x1b[0m`, client.id);
   client.emit('server', {
-    message: '!Socket.io de da la bienvenida!',
+    message: `¡Hola ${client.id}, Socket.io de da la bienvenida!`,
     path: join(__dirname, 'clientConnection.js'),
   });
 });
