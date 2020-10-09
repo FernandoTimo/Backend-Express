6afe5cfb4c538ee11ade40cc48def04fcd815fd0
FernandoTimo <Fertimo99@gmail.com>
2020-10-09 08:57:52 -0500

    Server

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import 'dotenv/config';
 import database from './database';
 import { server } from './server';
 
-const main = async () => {
-  server.listen(process.env.PORT, () => {
-    console.log(
-      `:: \x1b[36mSERVER\x1b[0m \x1b[33mhttp://localhost:${process.env.PORT}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
-    );
-  });
-  database.connection.once('open', () => {
-    console.log(
-      `:: \x1b[36mDATABASE\x1b[0m \x1b[33m${process.env.MONGODB_URI}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
-    );
-  });
-};
-main();
+server.listen(process.env.PORT, () => {
+  console.log(
+    `:: \x1b[36mSERVER\x1b[0m \x1b[33mhttp://localhost:${process.env.PORT}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
+  );
+});
+database.connection.once('open', () => {
+  console.log(
+    `:: \x1b[36mDATABASE\x1b[0m \x1b[33m${process.env.MONGODB_URI}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
+  );
+});
