aedc5dd01386917e360e1c4e4fe6f7db67aaf808
FernandoTimo <Fertimo99@gmail.com>
2020-10-18 12:43:15 -0500

    commit largo sin strings

--- src/sockets/clientConnection.js
+++ src/sockets/clientConnection.js
@@ -1,6 +1,5 @@
 import { socket } from './socket';
 import { join } from 'path';
-
 socket.on('connection', (client) => {
   console.log(`\x1b[31mNuevoCliente:\x1b[0m`, client.id);
   client.emit('server', {
