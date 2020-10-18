f78c8b4c9396467218bcd091d10b525a3c4b23d9
FernandoTimo <Fertimo99@gmail.com>
2020-10-18 08:40:15 -0500

    new ids generator

--- src/libraries/Identificador.js
+++ src/libraries/Identificador.js
@@ -1,37 +1,39 @@
 import fs from 'fs';
-
-export const Identificador = async (name) => {
-  if (typeof name !== 'string' || !name.trim?.())
-    throw new Error('Invalid name');
+/**
+ * Genera ids en base 62 consecutivamente.
+ * @param {String} label Nombre de la coleccion de IDs que servirá como referencia para esta coleccion
+ */
+export const Identificador = async (label) => {
+  if (typeof label !== 'string' || !label.trim?.())
+    throw new Error('Invalid label');
   const path = 'resources/Identificadores.json';
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync('resources', { recursive: true });
-    fs.writeFileSync(path, JSON.stringify({ [name]: 1 }));
+  if (!fs.existsSync(path) || fs.readFileSync(path).byteLength === 0) {
+    !fs.existsSync('resources') &&
+      fs.mkdirSync('resources', { recursive: true });
+    fs.writeFileSync(path, JSON.stringify({ [label]: 0 }));
+    return 0;
   }
-  console.log(!!!JSON.parse(fs.readFileSync(path)));
-  let global = JSON.parse(fs.readFileSync(path));
-  if (!global[name]) {
-    fs.writeFileSync(path, JSON.stringify({ [name]: 1 }));
-    return '1';
-  } else {
-    let num = global[name] + 1;
-    var order =
-      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var base = order.length;
-    var str = '',
-      r;
-    while (num) {
-      r = num % base;
-      num -= r;
-      num /= base;
-      str = order.charAt(r) + str;
-    }
-    global[name] += 1;
-    fs.writeFileSync(path, JSON.stringify(global));
-    return str;
+  const CharsList =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let ids = JSON.parse(fs.readFileSync(path));
+  ids[label] = ids[label] ?? -1;
+  let counter = ids[label] + 1,
+    str = '',
+    index;
+  while (counter) {
+    index = counter % CharsList.length;
+    str = CharsList.charAt(index) + str;
+    counter = (counter - index) / CharsList.length;
   }
+  ids[label] += 1;
+  fs.writeFileSync(path, JSON.stringify(ids));
+  return str;
 };
 
+/**
+ * Genera un ID con un tamaño máximo de 10 digitos
+ * @param {Number} lenght Cantidad de dígitos que tendrá el id. Máximo valor = 10
+ */
 export const customID = (lenght = 10) => {
   if (lenght > 10) {
     throw new Error('La longitud máxima es 10');
