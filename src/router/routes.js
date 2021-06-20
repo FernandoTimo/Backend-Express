db64dde012dbbab7b7487c725e361d470866faf2
FernandoTimo <fertimo99@gmail.com>
2021-06-20 08:50:11 -0500

    Middleware

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const routes = Router();
 
 import { Index, BadUrl } from '../controllers/controller';
-import { Auth } from '../middlewares/Auth';
+import { Auth } from '../middlewares/Auth.middleware';
 routes.route('/').get(Auth, Index);
 routes.route('*').get(BadUrl);
 
