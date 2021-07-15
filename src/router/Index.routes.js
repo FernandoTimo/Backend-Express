9e9c139d974c061739d4a98e2110852d2fd683ab
FernandoTimo <fertimo99@gmail.com>
2021-07-15 11:11:03 -0500

    Routes

--- src/router/Index.routes.js
+++ src/router/Index.routes.js
@@ -1,18 +1,13 @@
 import { Router } from 'express';
 const routes = Router();
 
-// CONTROLLERS
-import { Index, BadRequest } from '../controllers/global.controller';
-
 // MIDDLEWARES
 import { Auth } from '../middlewares/auth.middleware';
 
-// -- Index routes
+// ROUTES
+
+import { Index, BadRequest } from '../controllers/global.controller';
 routes.route('/').get(Auth, Index);
-
-// -- Global Routes
-
-// -- 404 Bad request route
 routes.route('*').get(BadRequest);
 
 export default routes;
