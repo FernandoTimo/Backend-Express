342c83ca6affe3e9ff21cfbbefffa5407c7ecf48
FernandoTimo <fertimo99@gmail.com>
2021-07-15 09:46:51 -0500

    Controllers

--- src/controllers/global.controller.js
+++ src/controllers/global.controller.js
@@ -1,23 +1,11 @@
-export const Index = async (req, res) => {
-  const method = req.method;
-  const protocol = req.protocol;
-  const host = req.headers.host;
-  const path = req.path;
-  const url = `${method} => ${protocol}://${host}${path}`;
+export async function Index(req, res) {
+  const { method, protocol, path, headers } = req;
+  const url = `${method} => ${protocol}://${headers.host}${path}`;
   let token = req.token;
-  res.status(200).json({
-    url: `${url}`,
-    token,
-  });
-};
-export const BadUrl = async (req, res) => {
-  const protocol = req.protocol;
-  const path = req.path;
-  const host = req.headers.host;
-  const method = req.method;
-  const url = `${method} => ${protocol}://${host}${path}`;
-  res.status(200).json({
-    url: `${url}`,
-    mensaje: 'Esta es una ruta NO PROTEGIDA por `middlewares/Auth.js`',
-  });
-};
+  res.status(200).json({ ok: true, url, token });
+}
+export async function BadUrl(req, res) {
+  const { method, protocol, path, headers } = req;
+  const url = `${method} => ${protocol}://${headers.host}${path}`;
+  res.status(404).json({ ok: false, url, mensaje: 'Bad request`' });
+}
