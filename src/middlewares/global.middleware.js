d837186572ef646ede9e4f2e8641f21b98dbd936
FernandoTimo <fertimo99@gmail.com>
2021-08-08 16:37:46 -0500

    Icon xd

--- src/middlewares/global.middleware.js
+++ src/middlewares/global.middleware.js
@@ -9,7 +9,7 @@ middleware.use(cors({ origin: process.env.CORS_CLIENT_HOST }));
 middleware.use(express.urlencoded({ extended: true }));
 middleware.use(express.json());
 middleware.use(
-  favicon(path.join(__dirname, '../../public/icons/fav.main.ico'))
+  favicon(path.join(__dirname, '../../public/icons/fav_main.ico'))
 );
 middleware.use(fileUpload());
 middleware.use('/p', express.static(path.join(__dirname, '../../public')));
