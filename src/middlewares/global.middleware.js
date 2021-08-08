b982a219c8923c8b0264645e11ecafa06ea687cb
FernandoTimo <fertimo99@gmail.com>
2021-08-08 16:33:55 -0500

    ENv

--- src/middlewares/global.middleware.js
+++ src/middlewares/global.middleware.js
@@ -8,7 +8,9 @@ import router from '../router/index.routes';
 middleware.use(cors({ origin: process.env.CORS_CLIENT_HOST }));
 middleware.use(express.urlencoded({ extended: true }));
 middleware.use(express.json());
-middleware.use(favicon(path.join(__dirname, '../../public/icons/favicon.ico')));
+middleware.use(
+  favicon(path.join(__dirname, '../../public/icons/fav.main.ico'))
+);
 middleware.use(fileUpload());
 middleware.use('/p', express.static(path.join(__dirname, '../../public')));
 middleware.use('/', router);
