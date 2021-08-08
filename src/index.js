a38b87df10eb826ba6da176ccd022d4714f3cd4b
FernandoTimo <fertimo99@gmail.com>
2021-08-08 15:28:07 -0500

    Index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill';
 import 'dotenv/config';
-import database from './src/database';
-import { server } from './src/server';
+import database from './database';
+import { server } from './server';
 
 server.listen(process.env.PORT, () => {
   console.log(`SERVER: http://localhost:${process.env.PORT}`);
