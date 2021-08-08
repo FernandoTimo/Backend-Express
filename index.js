d7219721c624eb607a070f9f5b9be626f392ffc2
FernandoTimo <fertimo99@gmail.com>
2021-08-08 12:42:39 -0500

    Logs

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ import database from './src/database';
 import { server } from './src/server';
 
 server.listen(process.env.PORT, () => {
-  console.log(
-    `:: \x1b[36mSERVER\x1b[0m \x1b[33mhttp://localhost:${process.env.PORT}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
-  );
+  console.log(`SERVER: http://localhost:${process.env.PORT}`);
 });
 database.connection.once('open', () => {
-  console.log(
-    `:: \x1b[36mDATABASE\x1b[0m \x1b[33m${process.env.MONGODB_URI}\x1b[0m \x1b[36mWORKS\x1b[0m ::`
-  );
+  console.log(`DATABASE: ${process.env.MONGODB_URI}`);
 });
